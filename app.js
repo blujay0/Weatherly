3118a2003b324506bbbf1a0ba5b541423dfbf7e1
Jay Lim <jlimoman@gmail.com>
2023-10-15 16:39:23 -0600

    add api key in script file

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  // import the dotenv library
+  require('dotenv').config();
+
+  // access the API key from the environment variable
+  const apiKey = process.env.WEATHER_API_KEY;
+
   const currentYear = new Date().getFullYear();
 
   document.getElementById("currentYear").innerHTML = currentYear;
