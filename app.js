930ecb7e8709dc81bd5fe791dc96e74137ea98cb
Jay Lim <jlimoman@gmail.com>
2023-10-24 04:29:30 -0600

    align content to be flush with left for hourly-card in today.html

--- app.js
+++ app.js
@@ -109,8 +109,8 @@ document.addEventListener('DOMContentLoaded', () => {
               <div class="hourly-card">
                 <p class="hour">${time}</p>
                 <img src="${weatherIcon}" alt="hourly weather symbol" class="hourly-icon">
-                <p class="weather">${condition}</p>
                 <p class="temp">${temperatureC} °C | ${temperatureF} °F </p>
+                <p class="weather">${condition}</p>
               </div>
             `;
 
