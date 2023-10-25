eacb191728aedf9518bb567275dc82b33c73d5fd
Jay Lim <jlimoman@gmail.com>
2023-10-24 22:22:30 -0600

    working weekly data render

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const currentWeatherContainer = document.querySelector('.current-data');
   const todayForecastContainer = document.querySelector('.today-data'); // Container for today's forecast
   const todayLocation = document.querySelector('.today-h2');
+  const weekForecastContainer = document.querySelector('.week-data');
 
   // Access the API key
   const apiKey = WEATHER_API_KEY;
@@ -16,6 +17,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // API endpoint for daily forecast data
   const todayApiEndpoint = "https://api.weatherapi.com/v1/forecast.json";
+
+  // API endpoint for week forecast data
+  const weekApiEndpoint = "https://api.weatherapi.com/v1/forecast.json";
+
 
   // For keeping the year updated in the footer
   document.getElementById("currentYear").innerHTML = currentYear;
@@ -149,33 +154,73 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   };
 
-  // Function to fetch daily forecast data and update HTML for today.html
-  const getWeeklyForecast = () => {
+  // Function to fetch week forecast data and update HTML for week.html
+  const getWeekForecast = () => {
     const search = searchInput.value.trim();
 
     if (search) {
-      const apiUrl = `${todayApiEndpoint}?key=${apiKey}&q=${search}&days=7&aqi=yes&alerts=yes`;
+      const apiUrl = `${weekApiEndpoint}?key=${apiKey}&q=${search}&days=7&aqi=yes&alert=yes`;
 
-    fetch(apiUrl)
-    .then(res => {
-      if (!res.ok) {
-        throw new Error('Network response was not ok'); // Handle HTTP error status
-      }
-      return res.json();
-    })
-    .then(data => {
+      fetch(apiUrl)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then((data) => {
+          const forecastDays = data.forecast.forecastday;
+          weekForecastContainer.innerHTML = ''; // clear previous data
 
+          forecastDays.forEach((dayData) => {
+            const date = dayData.date;
+            const maxTempC = dayData.day.maxtemp_c;
+            const maxTempF = dayData.day.maxtemp_f;
+            const minTempC = dayData.day.mintemp_c;
+            const minTempF = dayData.day.mintemp_f;
+            const condition = dayData.day.condition.text;
+            const weatherIcon = dayData.day.condition.icon;
+
+            // create a card for each day
+            const card = document.createElement('div');
+            card.classList.add('weekly-card');
+
+            const weekHtml = `
+              <div class="day-forecast">
+                <p class="date">${date}</p>
+                <p class="max-temp">${maxTempC}°C | ${maxTempF}°F</p>
+                <img src="${weatherIcon}" alt="weather symbol" class="weather-icon">
+                <p class="condition">${condition}</p>
+              </div>
+            `;
+
+            card.innerHTML = weekHtml;
+
+            weekForecastContainer.appendChild(card);
+          });
+        })
+        .catch((error) => {
+          // Handle errors
+          if (error.message === 'Network response was not ok') {
+            weekForecastContainer.innerHTML = '<p class="error-msg">Location not found. Please try again!</p>';
+          } else {
+            weekForecastContainer.innerHTML = '<p class="error-msg">An unknown error occurred. Please try again!</p>';
+          }
+        });
     }
+  };
 
   searchInput.addEventListener('keyup', (e) => {
     if (e.key === 'Enter') {
-      getCurrentWeather(); // For current.html
-      getDailyForecast(); // For today.html
+      getCurrentWeather(); // for current.html
+      getDailyForecast(); // for today.html
+      getWeekForecast(); // for week.html
     }
   });
 
   searchBtn.addEventListener('click', () => {
-    getCurrentWeather(); // For current.html
-    getDailyForecast(); // For today.html
+    getCurrentWeather(); // for current.html
+    getDailyForecast(); // for today.html
+    getWeekForecast(); // for week.html
   });
 });
