9d19d35e47fd2f24009792499f9b617ad7ebea46
Jay Lim <jlimoman@gmail.com>
2023-10-25 19:37:53 -0600

    add comments in js module

--- config/api-key.js
+++ config/api-key.js
@@ -1,1 +1,4 @@
+// either api-key.js module or .env file necessary to store api key, not both
+// using api-key.js module in this project for now
+
 export const WEATHER_API_KEY='de866a3309e94e6b970223908231510';
